aaa3fb55bdfdcd13c25ce78486d8525bb06acf76
EIPsilly <1020341547@qq.com>
2023-12-21 19:58:16 +0000

    PACS devnet

--- dataloaders/dataloader.py
+++ dataloaders/dataloader.py
@@ -1,18 +1,35 @@
 from datasets import mvtecad
 from torch.utils.data import DataLoader
 from dataloaders.utlis import worker_init_fn_seed, BalancedBatchSampler
-
+from datasets.PACS import PACS_Data
 
 def build_dataloader(args, **kwargs):
-    train_set = mvtecad.MVTecAD(args, train=True)
-    test_set = mvtecad.MVTecAD(args, train=False)
-    train_loader = DataLoader(train_set,
+
+    if args.data_name == "PACS":
+        data = PACS_Data(args)
+        train_set = data.train_data
+        train_loader = DataLoader(train_set,
                                 worker_init_fn=worker_init_fn_seed,
                                 batch_sampler=BalancedBatchSampler(args, train_set),
                                 **kwargs)
-    test_loader = DataLoader(test_set,
-                                batch_size=args.batch_size,
-                                shuffle=False,
-                                worker_init_fn=worker_init_fn_seed,
-                                **kwargs)
+        test_loader = {}
+        for key in data.test_dict:
+            test_loader[key] = DataLoader(data.test_dict[key],
+                                          batch_size=args.batch_size,
+                                          shuffle=False,
+                                          worker_init_fn=worker_init_fn_seed,
+                                          **kwargs)
+        
+    elif args.data_name == "MVTec":
+        train_set = mvtecad.MVTecAD(args, train=True)
+        test_set = mvtecad.MVTecAD(args, train=False)
+        train_loader = DataLoader(train_set,
+                                    worker_init_fn=worker_init_fn_seed,
+                                    batch_sampler=BalancedBatchSampler(args, train_set),
+                                    **kwargs)
+        test_loader = DataLoader(test_set,
+                                    batch_size=args.batch_size,
+                                    shuffle=False,
+                                    worker_init_fn=worker_init_fn_seed,
+                                    **kwargs)
     return train_loader, test_loader
