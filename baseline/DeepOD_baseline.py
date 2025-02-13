055b5b28bdd396c9d8a107c0cc8e2ac5ae1236a4
EIPsilly <1020341547@qq.com>
2025-02-13 17:14:40 +0000

    commit for transport

--- baseline/DeepOD_baseline.py
+++ baseline/DeepOD_baseline.py
@@ -23,8 +23,8 @@ with open("baseline/embedding_data.yaml", 'r', encoding="utf-8") as f:
     config = yaml.load(f.read(), Loader=yaml.FullLoader)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--data_name", type=str, default="MNIST_with_domain_label")
-parser.add_argument("--contamination_rate", type=float ,default=0.06)
+parser.add_argument("--data_name", type=str, default="MVTEC_with_domain_label")
+parser.add_argument("--contamination_rate", type=float ,default=0)
 parser.add_argument("--severity", type=int, default=3)
 parser.add_argument("--checkitew", type=str, default="bottle")
 parser.add_argument("--lr",type=float,default=0.0002)
@@ -54,15 +54,15 @@ parser.add_argument('--img_size', type=int, default=448, help="the image size of
 parser.add_argument("--save_embedding", type=int, default=0, help="No intermediate results are saved")
 
 parser.add_argument("--normal_class", nargs="+", type=int, default=[0])
-parser.add_argument("--anomaly_class", nargs="+", type=int, default=[1,2,3,4,5,6,7,8,9])
+parser.add_argument("--anomaly_class", nargs="+", type=int, default=[1,2,3,4,5,6])
 parser.add_argument("--n_anomaly", type=int, default=13, help="the number of anomaly data in training set")
 parser.add_argument("--n_scales", type=int, default=2, help="number of scales at which features are extracted")
-parser.add_argument('--backbone', type=str, default='DeepSAD', help="the backbone network")
+parser.add_argument('--backbone', type=str, default='RoSAS', help="the backbone network")
 parser.add_argument('--criterion', type=str, default='deviation', help="the loss function")
 parser.add_argument("--topk", type=float, default=0.1, help="the k percentage of instances in the topk module")
 parser.add_argument("--gpu",type=str, default="3")
 parser.add_argument("--results_save_path", type=str, default="/DEBUG")
-parser.add_argument("--domain_cnt", type=int, default=3)
+parser.add_argument("--domain_cnt", type=int, default=1)
 parser.add_argument("--use_AE", type=int, default=0)
 parser.add_argument("--method", type=int, default=0)
 parser.add_argument("--label_discount", type=float, default=1.0)
@@ -74,10 +74,13 @@ args.label_discount = int(8 * 27 / args.label_discount)
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
 
 if args.data_name.__contains__("PACS"):
-    data_epoch = config[f'{args.domain_cnt}domain']["PACS"]["epochs"][args.normal_class[0]]
-    data_lr = config[f'{args.domain_cnt}domain']["PACS"]["lr"][args.normal_class[0]]
-    if args.use_AE == 1:
-        data_name = f'method=0,backbone=wide_resnet50_2,domain_cnt={args.domain_cnt},normal_class={args.normal_class},anomaly_class={args.anomaly_class},epochs={data_epoch},lr={data_lr}'
+    if args.domain_cnt == 1:
+        data_epoch = config[f'{args.domain_cnt}domain']["PACS"]["epochs"][args.normal_class[0]]
+        data_lr = config[f'{args.domain_cnt}domain']["PACS"]["lr"][args.normal_class[0]]
+        if args.use_AE == 1:
+            data_name = f'method=0,backbone=wide_resnet50_2,domain_cnt={args.domain_cnt},normal_class={args.normal_class},anomaly_class={args.anomaly_class},epochs={data_epoch},lr={data_lr}'
+        else:
+            data_name = f'method=0,backbone=wide_resnet50_2,domain_cnt={args.domain_cnt},normal_class={args.normal_class},anomaly_class={args.anomaly_class}'
     else:
         data_name = f'method=0,backbone=wide_resnet50_2,domain_cnt={args.domain_cnt},normal_class={args.normal_class},anomaly_class={args.anomaly_class}'
     file_name = f'method={args.method},backbone={args.backbone},domain_cnt={args.domain_cnt},normal_class={args.normal_class},anomaly_class={args.anomaly_class},batch_size={args.batch_size},epochs={args.epochs},lr={args.lr},cnt={args.cnt}'
@@ -86,16 +89,19 @@ if args.data_name.__contains__("PACS"):
         file_name += f",contamination={args.contamination_rate}"
     if args.domain_cnt == 3:
         data = np.load(f'results/PACS_embedding/{data_name}.npz', allow_pickle=True)
-    if args.domain_cnt == 1:
+    elif args.domain_cnt == 1:
         if args.contamination_rate != 0:
             data = np.load(f'results/PACS_1domain_embedding_contamination/{data_name}.npz', allow_pickle=True)
         else:
-            data = np.load(f'results/PACS_1domain_encoder_embedding/{data_name}.npz', allow_pickle=True)
+            if args.use_AE == 1:
+                data = np.load(f'results/PACS_1domain_encoder_embedding/{data_name}.npz', allow_pickle=True)
+            else:
+                data = np.load(f'results/PACS_embedding/{data_name}.npz', allow_pickle=True)
     domain_list = ['photo', 'art_painting', 'cartoon', 'sketch']
 if args.data_name.__contains__("MVTEC"):
     data_name = f'method=0,backbone=wide_resnet50_2,domain_cnt={args.domain_cnt},checkitew={args.checkitew}'
     file_name = f'method={args.method},backbone={args.backbone},domain_cnt={args.domain_cnt},checkitew={args.checkitew},batch_size={args.batch_size},epochs={args.epochs},lr={args.lr},cnt={args.cnt}'
-    if args.domain_cnt == 3:
+    if args.domain_cnt == 4:
         data = np.load(f'results/MVTEC_embedding/{data_name}.npz', allow_pickle=True)
     if args.domain_cnt == 1:
         data = np.load(f'results/MVTEC_1domain_embedding/{data_name}.npz', allow_pickle=True)
